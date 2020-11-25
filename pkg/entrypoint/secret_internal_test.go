c010fe0aa38635fac514339195ec21d33374eedc
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2020-11-25 09:50:41 +0900

    test: add test

---
+++ pkg/entrypoint/secret_internal_test.go
@@ -0,0 +1,60 @@
+package entrypoint
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestEntrypoint_parseSecrets(t *testing.T) {
+	ep := Entrypoint{}
+	data := []struct {
+		title string
+		arr   []interface{}
+		exp   []Secret
+		isErr bool
+	}{
+		{
+			title: "string secret",
+			arr: []interface{}{
+				map[string]interface{}{
+					"name":   "foo",
+					"secret": "password",
+				},
+			},
+			exp: []Secret{
+				{
+					Name:   "foo",
+					Secret: "password",
+				},
+			},
+		},
+		{
+			title: "json secret",
+			arr: []interface{}{
+				map[string]interface{}{
+					"name":   "foo",
+					"secret": true,
+				},
+			},
+			exp: []Secret{
+				{
+					Name:   "foo",
+					Secret: `true`,
+				},
+			},
+		},
+	}
+	for _, d := range data {
+		d := d
+		t.Run(d.title, func(t *testing.T) {
+			secrets, err := ep.parseSecrets(d.arr)
+			if d.isErr {
+				require.NotNil(t, err)
+				return
+			}
+			require.Nil(t, err)
+			require.Equal(t, d.exp, secrets)
+		})
+	}
+}
